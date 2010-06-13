4ec90c78c4d7fe9e7d1730980992c549cfe9b6a6
sgry <sgry@b7cf00c4-1bf1-4e50-9238-a11fa35dd3c7>
2010-06-13 01:59:19 +0000

    tagged 1.5.6

---
+++ tags/1.5.6/build.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set _OPT=-nologo -v:m -t:Build
+set _OPT=%_OPT% -p:Configuration=Release
+set _OPT=%_OPT% -clp:ForceNoAlign;ShowCommandLine
+set _SOLUTION_FILE=%~1
+
+if "%~1"=="" (
+	echo # no solution file was specified; using All.vs8.sln.
+	set _SOLUTION_FILE=All.vs8.sln
+)
+
+:: build
+echo ===== begin msbuild =====
+msbuild.exe "%_SOLUTION_FILE%" %_OPT%
+echo ===== end msbuild =====
