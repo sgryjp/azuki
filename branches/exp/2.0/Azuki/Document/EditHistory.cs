6c3ab5817390644b135b668f8ae4cf6fd43ae4a0
sgry <sgry@b7cf00c4-1bf1-4e50-9238-a11fa35dd3c7>
2013-07-19 15:58:46 +0000

    created experimental 2.0 branch.

---
+++ branches/exp/2.0/Azuki/Document/EditHistory.cs
@@ -0,0 +1,262 @@
+﻿// file: EditHistory.cs
+// brief: History managemer for UNDO.
+// author: Suguru YAMAMOTO
+//=========================================================
+using System;
+
+namespace Sgry.Azuki
+{
+	/// <summary>
+	/// Stack object to hold editing actions.
+	/// </summary>
+	class EditHistory
+	{
+		#region Fields
+		const int GrowSize = 32;
+		EditAction[] _Stack;
+		int _NextIndex = 0;
+		EditAction _GroupingUndoChain = null;
+		EditAction _LastSavedAction = null;
+		int _GroupUndoDepth = 0;
+		#endregion
+
+		#region Init / Dispose
+		/// <summary>
+		/// Creates a new instance.
+		/// </summary>
+		public EditHistory()
+		{
+			_Stack = new EditAction[ GrowSize ];
+		}
+		#endregion
+
+		#region Operations
+		/// <summary>
+		/// Pushes new action to the stack.
+		/// </summary>
+		public void Add( EditAction action )
+		{
+			if( _GroupingUndoChain != null )
+			{
+				// put given action to the head of the chain
+				action.Next = _GroupingUndoChain;
+				_GroupingUndoChain = action;
+			}
+			else
+			{
+				// if there is no more space, expand buffer
+				if( _Stack.Length <= _NextIndex )
+				{
+					Utl.ResizeArray( ref _Stack, _Stack.Length + GrowSize );
+				}
+	
+				// stack up this action
+				_Stack[ _NextIndex ] = action;
+				_NextIndex++;
+				if( _NextIndex < _Stack.Length )
+				{
+					_Stack[ _NextIndex ] = null;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the action most recently done and remove it from stack.
+		/// </summary>
+		public EditAction GetUndoAction()
+		{
+			if( _NextIndex <= 0 )
+				return null;
+
+			// return an action which is on top logically of the stack
+			_NextIndex--;
+			return _Stack[ _NextIndex ];
+		}
+
+		/// <summary>
+		/// Gets the action most recently done and remove it from stack.
+		/// </summary>
+		public EditAction GetRedoAction()
+		{
+			if( _NextIndex < _Stack.Length
+				&& _Stack[_NextIndex] != null )
+			{
+				EditAction redoAction = _Stack[ _NextIndex ];
+				_NextIndex++;
+				return redoAction;
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Clears all containing actions.
+		/// </summary>
+		public void Clear()
+		{
+			_NextIndex = 0;
+
+			// (all references must be nullified to allow GC collecting them)
+			for( int i=0; i<_Stack.Length; i++ )
+			{
+				_Stack[i] = null;
+			}
+		}
+
+		/// <summary>
+		/// Begins grouping up editing actions into a single UNDO action.
+		/// </summary>
+		public void BeginUndo()
+		{
+			if( _GroupingUndoChain == null )
+			{
+				_GroupingUndoChain = new EditAction( null,
+													 0, null, null,
+													 0, 0, null );
+			}
+			_GroupUndoDepth++;
+		}
+
+		/// <summary>
+		/// Ends grouping up editing actions.
+		/// </summary>
+		public void EndUndo()
+		{
+			_GroupUndoDepth--;
+			if( _GroupUndoDepth <= 0 )
+			{
+				if( _GroupingUndoChain != null )
+				{
+					EditAction groupedAction = _GroupingUndoChain;
+					_GroupingUndoChain = null;
+					if( groupedAction.HasNoEffect == false )
+					{
+						Add( groupedAction );
+					}
+				}
+				_GroupUndoDepth = 0;
+			}
+		}
+
+		public bool IsSavedState
+		{
+			get
+			{
+				// if Azuki is grouping one or more actions,
+				// it is safe to say the state must change
+				// regardless whether current (before concluding grouped UNDO) state
+				// is saved-state or not.
+				if( _GroupingUndoChain != null && _GroupingUndoChain.Next != null )
+				{
+					return false;
+				}
+
+				if( 0 < _NextIndex )
+				{
+					return (_LastSavedAction == _Stack[_NextIndex-1]);
+				}
+				else
+				{
+					return (_LastSavedAction == null);
+				}
+			}
+		}
+
+		public void SetSavedState()
+		{
+			if( 0 < _NextIndex )
+			{
+				_LastSavedAction = _Stack[_NextIndex-1];
+			}
+			else
+			{
+				_LastSavedAction = null;
+			}
+		}
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Whether an available UNDO action exists or not.
+		/// </summary>
+		public bool CanUndo
+		{
+			get
+			{
+				if( 0 < _NextIndex )
+				{
+					// UNDOable action exists
+					return true;
+				}
+				else if( IsGroupingActions )
+				{
+					// group UNDO is going on
+					// so there will be an UNDOable action when it ends
+					return true;
+				}
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Whether an available REDO action exists or not.
+		/// </summary>
+		public bool CanRedo
+		{
+			get
+			{
+				return (_NextIndex < _Stack.Length
+					&& _Stack[_NextIndex] != null);
+			}
+		}
+
+		/// <summary>
+		/// Whether group UNDO/REDO is executing or not.
+		/// </summary>
+		public bool IsGroupingActions
+		{
+			get{ return (_GroupingUndoChain != null); }
+		}
+
+		/// <summary>
+		/// Gets estimated memory size used by this object.
+		/// </summary>
+		public int MemoryUsage
+		{
+			get
+			{
+				int usage = 0;
+
+				foreach( EditAction action in _Stack )
+				{
+					if( action != null )
+					{
+						usage += action.InsertedText.Length * sizeof(char);
+						usage += action.DeletedText.Length * sizeof(char);
+					}
+				}
+
+				return usage;
+			}
+		}
+		#endregion
+
+		#region Utilities
+		static class Utl
+		{
+			public static void ResizeArray<T>( ref T[] array, int newSize )
+			{
+				T[] value = new T[ newSize ];
+				int minSize = Math.Min( array.Length, newSize );
+				
+				if( 0 < minSize )
+				{
+					Array.Copy( array, value, minSize );
+				}
+
+				array = value;
+			}
+		}
+		#endregion
+	}
+}
---
+++ branches/exp/2.0/Azuki/View/TextDecoration.cs
@@ -0,0 +1,158 @@
+﻿// file: TextDecoration.cs
+// brief: Text decoration classes.
+//=========================================================
+using System;
+using System.Drawing;
+
+namespace Sgry.Azuki
+{
+	/// <summary>
+	/// Represents how text should be decorated graphically.
+	/// </summary>
+	public class TextDecoration
+	{
+		static TextDecoration _None = null;
+
+		#region Init / Dispose
+		/// <summary>
+		/// Creates a new instance.
+		/// </summary>
+		protected TextDecoration()
+		{}
+		#endregion
+
+		/// <summary>
+		/// Text should not be decorated.
+		/// </summary>
+		public static TextDecoration None
+		{
+			get
+			{
+				if( _None == null )
+				{
+					_None = new TextDecoration();
+				}
+				return _None;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Represents how text should be decorated with special background color.
+	/// </summary>
+	public class BgColorTextDecoration : TextDecoration
+	{
+		Color bgColor;
+
+		#region Init / Dispose
+		/// <summary>
+		/// Creates a new instance.
+		/// </summary>
+		public BgColorTextDecoration( Color backgroundColor )
+		{
+			bgColor = backgroundColor;
+		}
+		#endregion
+
+		/// <summary>
+		/// Gets the background color of decorated tokens.
+		/// </summary>
+		public Color BackgroundColor
+		{
+			get{ return bgColor; }
+		}
+	}
+
+	/// <summary>
+	/// Represents how text should be decorated with underline.
+	/// </summary>
+	public class UnderlineTextDecoration : TextDecoration
+	{
+		LineStyle _LineStyle;
+		Color _LineColor;
+
+		/// <summary>
+		/// Creates a new instance.
+		/// </summary>
+		/// <param name="lineStyle">Style of the underline.</param>
+		/// <param name="lineColor">
+		/// 	The color used to draw the underline.
+		/// 	If Color.Transparent is specified, underline will be drawn in same color
+		/// 	as the text part.
+		/// </param>
+		public UnderlineTextDecoration( LineStyle lineStyle, Color lineColor )
+		{
+			_LineStyle = lineStyle;
+			_LineColor = lineColor;
+		}
+
+		/// <summary>
+		/// Gets or sets style of underline.
+		/// </summary>
+		public LineStyle LineStyle
+		{
+			get{ return _LineStyle; }
+			set{ _LineStyle = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets color of underline.
+		/// </summary>
+		public Color LineColor
+		{
+			get{ return _LineColor; }
+			set{ _LineColor = value; }
+		}
+	}
+
+	/// <summary>
+	/// - EXPERIMENTAL - Decorates text with a transparent rectangle.
+	/// </summary>
+	public class OutlineTextDecoration : TextDecoration
+	{
+		Color _LineColor;
+
+		/// <summary>
+		/// Creates a new instance.
+		/// </summary>
+		/// <param name="outlineColor">The color of the outline.</param>
+		public OutlineTextDecoration( Color outlineColor )
+		{
+			LineColor = outlineColor;
+		}
+
+		/// <summary>
+		/// The color of the outline.
+		/// </summary>
+		public Color LineColor
+		{
+			get{ return _LineColor; }
+			set{ _LineColor = value; }
+		}
+	}
+
+	/// <summary>
+	/// Indicates style of line for text decoration.
+	/// </summary>
+	public enum LineStyle
+	{
+		/// <summary>Does not draw line.</summary>
+		None,
+
+		/// <summary>Solid line.</summary>
+		Solid,
+
+		/// <summary>Doubled line.</summary>
+		Double,
+
+		/// <summary>Dashed line.</summary>
+		Dashed,
+
+		/// <summary>Line written with many dots.</summary>
+		Dotted,
+
+		/// <summary>Waved line.</summary>
+		Waved
+	}
+
+}
