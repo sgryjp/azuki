b483e659a9007c609443d42ce90267f47d844a99
sgry <sgry@b7cf00c4-1bf1-4e50-9238-a11fa35dd3c7>
2013-08-11 02:00:06 +0000

    (2.0枝)キャレットの前にカッコがある場合にもそれに対応するカッコをハイライトするように

--- branches/exp/2.0/Azuki/UiImpl.cs
+++ branches/exp/2.0/Azuki/UiImpl.cs
@@ -1250,6 +1250,7 @@ namespace Sgry.Azuki
 		public void Doc_ContentChanged( object sender, ContentChangedEventArgs e )
 		{
 			Debug.Assert( _IsDisposed == false );
+			ViewParam param = Document.ViewParam;
 
 			// delegate to marker objects
 			if( _Document.MarksUri )
@@ -1265,23 +1266,18 @@ namespace Sgry.Azuki
 			_UI.UpdateCaretGraphic();
 
 			// update matched bracket positions
-			if( e.Index + e.OldText.Length < _Document.ViewParam.IndexOfBracketBeforeCaret )
+			for( int i=0; i<param.MatchedBracketIndexes.Length; i++ )
 			{
-				_Document.ViewParam.IndexOfBracketBeforeCaret =
-					_Document.ViewParam.IndexOfBracketBeforeCaret - e.OldText.Length + e.NewText.Length;
-			}
-			else if( e.Index <= _Document.ViewParam.IndexOfBracketBeforeCaret )
-			{
-				_Document.ViewParam.IndexOfBracketBeforeCaret = e.Index + e.NewText.Length;
-			}
-			if( e.Index + e.OldText.Length < _Document.ViewParam.IndexOfMatchedBracketBeforeCaret )
-			{
-				_Document.ViewParam.IndexOfMatchedBracketBeforeCaret =
-					_Document.ViewParam.IndexOfMatchedBracketBeforeCaret - e.OldText.Length + e.NewText.Length;
-			}
-			else if( e.Index <= _Document.ViewParam.IndexOfMatchedBracketBeforeCaret )
-			{
-				_Document.ViewParam.IndexOfMatchedBracketBeforeCaret = e.Index + e.NewText.Length;
+				if( e.Index + e.OldText.Length < param.MatchedBracketIndexes[i] )
+				{
+					param.MatchedBracketIndexes[i] = param.MatchedBracketIndexes[i]
+													 - e.OldText.Length
+													 + e.NewText.Length;
+				}
+				else if( e.Index <= param.MatchedBracketIndexes[i] )
+				{
+					param.MatchedBracketIndexes[i] = e.Index + e.NewText.Length;
+				}
 			}
 			UpdateMatchedBracketPosition();
 
@@ -1289,7 +1285,6 @@ namespace Sgry.Azuki
 			_UI.UpdateScrollBarRange();
 
 			// update range of text which should be highlighted
-			ViewParam param = Document.ViewParam;
 			if( e.Index < param.H_InvalidRangeBegin )
 			{
 				param.H_InvalidRangeBegin = e.Index;
@@ -1321,18 +1316,27 @@ namespace Sgry.Azuki
 
 		void UpdateMatchedBracketPosition()
 		{
+			UpdateMatchedBracketPosition( _Document.CaretIndex, true );
+			UpdateMatchedBracketPosition( _Document.CaretIndex-1, false );
+		}
+
+		void UpdateMatchedBracketPosition( int bracketIndex, bool afterCaret )
+		{
+			ViewParam param = _Document.ViewParam;
+			int offset = afterCaret ? 0 : 2;
+
 			// Reset matched bracket positions
-			int oldIbbc = _Document.ViewParam.IndexOfBracketBeforeCaret;
-			int oldImbbc = _Document.ViewParam.IndexOfMatchedBracketBeforeCaret;
-			_Document.ViewParam.IndexOfBracketBeforeCaret = -1;
-			_Document.ViewParam.IndexOfMatchedBracketBeforeCaret = -1;
+			int oldIbbc = param.MatchedBracketIndexes[offset];
+			int oldImbbc = param.MatchedBracketIndexes[offset+1];
+			param.MatchedBracketIndexes[offset] = -1;
+			param.MatchedBracketIndexes[offset+1] = -1;
 			if( View.HighlightsMatchedBracket == false )
 			{
 				return;
 			}
 
 			// Find matched brackets
-			int newIbbc = _Document.CaretIndex;
+			int newIbbc = bracketIndex;
 			int newImbbc = _Document.FindMatchedBracket( newIbbc, MaxMatchedBracketSearchLength );
 
 			// Update matched bracket positions and graphics
@@ -1352,9 +1356,9 @@ namespace Sgry.Azuki
 					View.Invalidate( newImbbc, newImbbc+1 );
 
 				// Update matched bracket positions
-				_Document.ViewParam.IndexOfMatchedBracketBeforeCaret = newImbbc;
+				param.MatchedBracketIndexes[offset+1] = newImbbc;
 				if( 0 <= newImbbc )
-					_Document.ViewParam.IndexOfBracketBeforeCaret = newIbbc;
+					param.MatchedBracketIndexes[offset] = newIbbc;
 			}
 		}
 		#endregion
