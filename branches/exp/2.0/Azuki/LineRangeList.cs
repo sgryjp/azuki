303cc6340251d27ab1009c5d079c01340d12cbae
sgry <sgry@b7cf00c4-1bf1-4e50-9238-a11fa35dd3c7>
2013-08-21 14:12:38 +0000

    Enumerator関連の処理を、yieldを使って書き直した

--- branches/exp/2.0/Azuki/LineRangeList.cs
+++ branches/exp/2.0/Azuki/LineRangeList.cs
@@ -48,50 +48,13 @@ namespace Sgry.Azuki
 		#region IEnumerator
 		public IEnumerator<ILineRange> GetEnumerator()
 		{
-			return new Enumerator(this);
+			for( int i=0; i<Count; i++ )
+				yield return this[i];
 		}
 
 		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
 		{
-			return new Enumerator(this);
-		}
-
-		class Enumerator : IEnumerator<ILineRange>
-		{
-			LineRangeList _list;
-			int _index = -1;
-
-			public Enumerator( LineRangeList list )
-			{
-				_list = list;
-			}
-
-			public ILineRange Current
-			{
-				get{ return _list[_index]; }
-			}
-
-			public void Dispose()
-			{}
-
-			object System.Collections.IEnumerator.Current
-			{
-				get{ return _list[_index]; }
-			}
-
-			public bool MoveNext()
-			{
-				if( _list.Count <= _index+1 )
-					return false;
-
-				_index++;
-				return true;
-			}
-
-			public void Reset()
-			{
-				_index = -1;
-			}
+			return GetEnumerator();
 		}
 		#endregion
 	}
