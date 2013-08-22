91e3f742fce9ed83d75d6ea4c83f19e0d8b36a12
Suguru Yamamoto <sgryjp@gmail.com>
2013-08-22 15:33:40 +0000

    ILineRangeListにIRangeListを実装させた

--- 2.0/Azuki/ILineRangeList.cs
+++ 2.0/Azuki/ILineRangeList.cs
@@ -6,29 +6,21 @@ namespace Sgry.Azuki
 	/// <summary>
 	/// An interface to extract ranges of lines.
 	/// </summary>
-	public interface ILineRangeList : IEnumerable<ILineRange>
+	public interface ILineRangeList : IRangeList, IEnumerable<ILineRange>
 	{
 		/// <summary>
-		/// Gets a range of a line at specifed index.
+		/// Gets a range of a line at a specifed index.
 		/// </summary>
 		/// <exception cref="ArgumentOutOfRangeException"/>
-		ILineRange this[ int lineIndex ]
+		new ILineRange this[ int lineIndex ]
 		{
 			get;
 		}
 
 		/// <summary>
-		/// Gets a range which contains a character at specified offset.
+		/// Gets a range of a line which contains a character at a specified char-index.
 		/// </summary>
 		/// <exception cref="ArgumentOutOfRangeException"/>
 		ILineRange AtOffset( int charIndex );
-
-		/// <summary>
-		/// Gets number of lines.
-		/// </summary>
-		int Count
-		{
-			get;
-		}
 	}
 }
