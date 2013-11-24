5a973c4b3e13530fc280a496eba4fa563679fc27
Suguru Yamamoto <sgryjp@gmail.com>
2013-11-24 08:22:14 +0000

    グループUndoを、BeginUndoとEndUndoの呼び出しでまとめるのではなく、BeginUndoが返すcontextオブジェクトをusing文で管理する形に改善。

---
+++ 2.0/Azuki/UndoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sgry.Azuki
+{
+	class UndoContext : IDisposable
+	{
+		readonly Document _Document;
+
+		public UndoContext( Document doc )
+		{
+			_Document = doc;
+		}
+
+		public void Dispose()
+		{
+			_Document.EndUndo();
+		}
+	}
+}
