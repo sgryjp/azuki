7a466d74fa831a1397f8e0cd0fdfe6b4dcf21c22
sgry <sgry@b7cf00c4-1bf1-4e50-9238-a11fa35dd3c7>
2014-06-07 10:42:41 +0000

    tagged 1.7.10

---
+++ tags/1.7.10/Azuki/Highlighter/Coco/Buffer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sgry.Azuki.Highlighter.Coco
+{
+	class Buffer
+	{
+		public const int EOF = Char.MaxValue + 1;
+		Document	_Document;
+		int			_Position;
+		int			_EndPosition;
+
+		public Buffer( Document doc, int startIndex, int endIndex )
+		{
+			_Document = doc;
+			_Position = startIndex;
+			_EndPosition = endIndex;
+		}
+
+		public int Read()
+		{
+			if( _Position < _Document.Length || _Position < _EndPosition )
+				return _Document[ _Position++ ];
+			else
+				return EOF;
+		}
+
+		public int Peek()
+		{
+			if( _Position < _Document.Length || _Position < _EndPosition )
+				return _Document[ _Position ];
+			else
+				return EOF;
+		}
+
+		public int Pos
+		{
+			get{ return _Position; }
+			set
+			{
+				if( _Position < 0 || _EndPosition < _Position )
+					throw new ArgumentOutOfRangeException();
+
+				_Position = value;
+			}
+		}
+
+		public string GetString( int begin, int end )
+		{
+			return _Document.GetTextInRange( begin, end );
+		}
+	}
+}
---
+++ tags/1.7.10/Azuki/View/MouseCursor.cs
@@ -0,0 +1,38 @@
+﻿// file: MouseCursor.cs
+// brief: Type of mouse cursor's graphic.
+// author: YAMAMOTO Suguru
+// update: 2010-11-28
+//=========================================================
+
+namespace Sgry.Azuki
+{
+	/// <summary>
+	/// Type of mouse cursor's graphic.
+	/// </summary>
+	public enum MouseCursor
+	{
+		/// <summary>
+		/// Arrow pointing up and to the left.
+		/// Typically used when none of other cursor type is apropriate to be used.
+		/// </summary>
+		Arrow,
+
+		/// <summary>
+		/// Graphic of capital alhpabet letter 'I'.
+		/// Typical usage is expressing clicking can set caret to where the cursor is at.
+		/// </summary>
+		IBeam,
+
+		/// <summary>
+		/// Hand pointing up.
+		/// Typical usage is expressing the cursor is on something which can react on click.
+		/// </summary>
+		Hand,
+
+		/// <summary>
+		/// Arrow pointing up.
+		/// Typical usage is expressing moving data.
+		/// </summary>
+		DragAndDrop
+	}
+}
